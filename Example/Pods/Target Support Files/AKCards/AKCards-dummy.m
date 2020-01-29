c29927731651922d4a8128fc366f6db9f2bf28df
Андрей Короленко <korolenko.andrey.g@gmail.com>
2020-01-29 12:03:43 +0300

    Initial commit with cocoapod lib template

---
+++ Example/Pods/Target Support Files/AKCards/AKCards-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_AKCards : NSObject
+@end
+@implementation PodsDummy_AKCards
+@end
---
+++ Source/AKCards.swift
@@ -0,0 +1,17 @@
+//
+//  AKCards.swift
+//  
+//
+//  Created by Андрей Короленко on 29.01.2020.
+//
+
+public class AKCards: NSObject {
+
+    class public func openCard() {
+        
+    }
+    
+    class public func closeCard() {
+        
+    }
+}
